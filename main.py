955ad5cd234688e8919bbb2ef423127e09b2a4ef
Votizlov <SeriousFox@yandex.ru>
2020-03-20 10:38:47 +0300

    Done

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
+import re
+
 f = open("Input/1.txt", "r")
-f1 = f.readlines()
+f1 = f.readline()
 
 
-def reverse_text(lines):
-    res = ""
-    for i in lines:
-        resLine = ""
-        line = i.split()
-        for j in line:
-            resLine += (j[::-1])
-            resLine += " "
-        res += resLine
-        res += "\n"
-    return str(res)
+def format_text(line):
+    return re.sub(r'(\w+)\s+=\s+\1\s\+\s+1', r'\1++', line)
 
 
-print(reverse_text(f1))
 f = open("Output/1.txt", "w+")
-f.write(reverse_text(f1))
+f.write(format_text(f1))
