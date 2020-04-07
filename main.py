af2defc4fbeaa5c08fcd11e2b311d3636e82d315
votizlov <35692043+votizlov@users.noreply.github.com>
2020-04-07 23:24:22 +0300

    fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import re
 
-f = open("Input/1.txt", "r")
-f1 = f.readlines()
-f = open("Input/text.txt","r")
+f = open("Input/text.txt", "r")
+f1 = f.readline()
+f2 = open("Input/1.txt", "r")
+keys = f2.readlines()
 
 
 def format_text(key):
-    return re.findall(rf'\s+{key}\s+', f.readline())
+    return re.findall(rf'\s+{key}\s+', f1)
 
 
 f = open("Output/1.txt", "w+")
-for line in f1:
+for line in keys:
     for key in line.split():
-        print(key)
-        f.write(str(format_text(key)))
+        print(re.findall(rf'\s+{key}\s+', f1))
+        f.write(', '.join(re.findall(rf'\s+{key}\s+', f1)))
