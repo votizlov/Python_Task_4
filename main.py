9a802d0e58b959a0044510e878f009e3415ff3c4
votizlov <35692043+votizlov@users.noreply.github.com>
2020-06-04 16:16:36 +0300

    done

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import re
+import textwrap
 
 f = open("Input/text.txt", "r")
 f1 = f.readlines()
 f2 = open("Input/1.txt", "r")
 keys = f2.readlines()
 
-
-def format_text(key, l):
-    return re.findall(rf'\s+{key}\s+', l)
-
+textWith = 10
 
 f = open("Output/1.txt", "w+")
-for line in keys:
-    for key in line.split():
-        for l in f1:
-            f.write(', '.join(re.findall(rf'\s+{key}\s+', l)))
+for l in f1:
+    f.write("\n" + "      " + textwrap.fill(l,textWith))
